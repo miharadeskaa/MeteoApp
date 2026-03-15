e53a9598f0096e7f5bc8efe0b3cc5571c3cd2116
miharadeskaa <sillimika@hotmail.com>
2026-03-15 12:08:23 +0100

    update password

--- Services/login.cs
+++ Services/login.cs
@@ -5,14 +5,21 @@ namespace MeteoApp.Services
         public bool IsLoggedIn { get; private set; } = false;
         public string UserName { get; private set; } = "";
 
-        
         public event Action? OnChange;
 
-        public void Login(string name)
+        // On a ajouté le paramètre password
+        public bool Login(string name, string password)
         {
-            IsLoggedIn = true;
-            UserName = name;
-            NotifyStateChanged();
+            // Vérification simple (tu peux changer "1234")
+            if (!string.IsNullOrWhiteSpace(name) && password == "1234")
+            {
+                IsLoggedIn = true;
+                UserName = name;
+                NotifyStateChanged();
+                return true; // Connexion réussie
+            }
+            
+            return false; // Connexion échouée
         }
 
         public void Logout()
