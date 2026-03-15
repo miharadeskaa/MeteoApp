f182a8448217ca80f3e010fce2cf26dd0053564f
miharadeskaa <sillimika@hotmail.com>
2026-03-15 12:00:40 +0100

    Update API

--- Services/FavorisServices.cs
+++ Services/FavorisServices.cs
@@ -3,6 +3,7 @@ using MeteoApp.Models;
 
 namespace MeteoApp.Services
 {
+// Ce service gère les favoris de l'utilisateur en utilisant le stockage local du navigateur (localStorage)
     public class FavorisServices
     {
         private readonly ILocalStorageService _localStorage;
@@ -13,21 +14,21 @@ namespace MeteoApp.Services
             _localStorage = localStorage;
         }
 
-        // 1. RÉCUPÉRER : On ne filtre que les favoris de l'utilisateur connecté
+// 1. RÉCUPÉRER : On ne filtre que les favoris de l'utilisateur connecté
         public async Task<List<Favoris>> GetFavorisAsync(string userName)
         {
             var allFavoris = await _localStorage.GetItemAsync<List<Favoris>>(StorageKey) ?? new List<Favoris>();
             return allFavoris.Where(f => f.UserName == userName).ToList();
         }
 
-        // 2. ENREGISTRER / MODIFIER
+// 2. ENREGISTRER / MODIFIER
         public async Task SaveFavoriteAsync(Favoris favori)
         {
             var allFavoris = await _localStorage.GetItemAsync<List<Favoris>>(StorageKey) ?? new List<Favoris>();
 
-            // On cherche si ce favori (même ID) existe déjà
+            // On vérifie s'il existe un favori pour l utilisateur
             var index = allFavoris.FindIndex(f => f.Id == favori.Id);
-
+            
             if (index != -1)
             {
                 // Mise à jour (Update)
@@ -42,7 +43,7 @@ namespace MeteoApp.Services
             await _localStorage.SetItemAsync(StorageKey, allFavoris);
         }
 
-        // 3. SUPPRIMER
+// 3. SUPPRIMER
         public async Task DeleteFavoriteAsync(Guid id)
         {
             var allFavoris = await _localStorage.GetItemAsync<List<Favoris>>(StorageKey) ?? new List<Favoris>();
